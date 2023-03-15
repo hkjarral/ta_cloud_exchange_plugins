d669716aa1c00c33fc4863540b96234f38504e65
Hkjarral <haider.jarral@illumio.com>
2023-03-15 15:44:06 -0700

    Commit 1

---
+++ Illumio/main.py
@@ -0,0 +1,82 @@
+import requests
+import json
+import argparse
+import ta_common
+import ta_cloud_exchange_plugin_helper
+
+# Set the Illumio API endpoint and authentication parameters
+API_ENDPOINT = 'https://api.illumio.com'
+API_KEY = '<your API key>'
+API_SECRET = '<your API secret>'
+
+# Define a function to authenticate with the API and return an access token
+def get_access_token():
+    auth_url = API_ENDPOINT + '/oauth2/token'
+    auth_data = {
+        'grant_type': 'client_credentials',
+        'client_id': API_KEY,
+        'client_secret': API_SECRET
+    }
+    response = requests.post(auth_url, data=auth_data)
+    if response.status_code == 200:
+        return response.json()['access_token']
+    else:
+        print('Authentication failed: ' + str(response.status_code))
+        return None
+
+# Define a function to retrieve a list of labels from the Illumio API
+def get_labels():
+    labels_url = API_ENDPOINT + '/api/v2/orgs/self/labels'
+    headers = {
+        'Authorization': 'Bearer ' + get_access_token(),
+        'Content-Type': 'application/json'
+    }
+    response = requests.get(labels_url, headers=headers)
+    if response.status_code == 200:
+        return response.json()
+    else:
+        print('Error retrieving labels: ' + str(response.status_code))
+        return None
+
+# Define the main function
+def main():
+    # Parse command-line arguments
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--client-id', required=True, help='OAuth2 client ID')
+    parser.add_argument('--client-secret', required=True, help='OAuth2 client secret')
+    args = parser.parse_args()
+
+    # Set the Netskope Cloud Exchange API endpoint and authentication parameters
+    CLOUD_EXCHANGE_API_ENDPOINT = 'https://api.netskope.com/cloudexchange/v1'
+    CLIENT_ID = args.client_id
+    CLIENT_SECRET = args.client_secret
+
+    # Authenticate with the Netskope Cloud Exchange API
+    cloud_exchange_auth = ta_cloud_exchange_plugin_helper.CloudExchangeAuth(CLOUD_EXCHANGE_API_ENDPOINT, CLIENT_ID, 
+CLIENT_SECRET)
+    access_token = cloud_exchange_auth.get_access_token()
+
+    # Retrieve a list of labels from the Illumio API
+    labels = get_labels()
+    if labels:
+        # Iterate over the labels and generate events for each one
+        for label in labels:
+            event = ta_cloud_exchange_plugin_helper.create_event(
+                'illumio_label',
+                {
+                    'id': label['id'],
+                    'name': label['name'],
+                    'description': label.get('description', ''),
+                    'created_at': label['created_at'],
+                    'updated_at': label['updated_at']
+                },
+                'illumio'
+            )
+            # Ingest the event into the Netskope Security Cloud platform
+            ta_cloud_exchange_plugin_helper.ingest_event(CLOUD_EXCHANGE_API_ENDPOINT, access_token, event)
+
+# Call the main function
+if __name__ == '__main__':
+    main()
+
+
