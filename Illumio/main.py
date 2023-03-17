c85b5dd2cf3f903aee9231018b857e522f705acb
Haider Jarral <42726887+hkjarral@users.noreply.github.com>
2023-03-17 12:10:01 -0700

    Update main.py

--- Illumio/main.py
+++ Illumio/main.py
@@ -1,96 +1,48 @@
 """Illumio Plugin implementation to pull data from Illumio Platform."""
 
-import requests
-import datetime
-import time
-from datetime import timedelta
-from requests.auth import HTTPBasicAuth
-import json
-from typing import List
-
-
-from netskope.integrations.cte.plugin_base import (
-    PluginBase,
-    ValidationResult,
-)
-class AuthenticationException(Exception):
-    pass
-
+from netskope_integrations import PluginBase, PluginOptions
+from data_objects import ValidationResult
+from illuminet.client import IllumioApiClient
+from illuminet.config import IllumioConfig
+from illuminet.data import IllumioData
 
 class IllumioPlugin(PluginBase):
-    """IllumioPlugin class for pulling threat information."""
+    def __init__(self):
+        self.config = IllumioConfig()
+        self.client = IllumioApiClient(self.config)
+        self.data = None
 
-    def handle_error(self, resp):
-        """Handle the different HTTP response code.
+    def init(self, options):
+        self.config.init_config(options)
 
-        Args:
-            resp (requests.models.Response): Response object
-            returned from API call.
-        Returns:
-            dict: Returns the dictionary of response JSON
-            when the response code is 200.
-        Raises:
-            HTTPError: When the response code is not 200.
-        """
-        if resp.status_code == 200 or resp.status_code == 201:
-            try:
-                return resp.json()
-            except ValueError:
-                self.notifier.error(
-                    "Plugin: Illumio, "
-                    "Exception occurred while parsing JSON response."
-                )
-                self.logger.error(
-                    "Plugin: Illumio, "
-                    "Exception occurred while parsing JSON response."
-                )
-        if resp.status_code == 204:
-            return {}
-        elif resp.status_code == 400:
-            auth_reponse = resp.text
-            result_dict = json.loads(auth_reponse)
-            err_msg = result_dict["error"]
-            if(
-                "errorMessage" in result_dict
-                and err_msg == "invalid_basic_auth"
-            ):
-                raise AuthenticationException(
-                    "Invalid Key ID or Key Secret Provided."
-                )
-            else:
-                self.notifier.error(
-                        "Plugin: Illumio, "
-                        "Received exit code 400, Bad Request."
-                    )
-                self.logger.error(
-                        "Plugin: Illumio, "
-                        "Received exit code 400, Bad Request"
-                )
-        elif resp.status_code == 403:
-            self.notifier.error(
-                "Plugin: Illumio, "
-                "Received exit code 403, Forbidden User"
-            )
-            self.logger.error(
-                "Plugin: Illumio, "
-                "Received exit code 403, Forbidden User"
-            )
-        elif resp.status_code >= 500 and resp.status_code < 600:
-            self.notifier.error(
-                f"Plugin: Illumio, "
-                f"Received exit code {resp.status_code}, HTTP server Error"
-            )
-            self.logger.error(
-                f"Plugin: Illumio, "
-                f"Received exit code {resp.status_code}, HTTP server Error"
-            )
-        else:
-            self.notifier.error(
-                f"Plugin: Illumio, "
-                f"Received exit code {resp.status_code}, HTTP Error"
-            )
-            self.logger.error(
-                f"Plugin: Illumio, "
-                f"Received exit code {resp.status_code}, HTTP Error"
-            )
-        resp.raise_for_status()
+    def validate(self):
+        if not self.config.api_key:
+            return ValidationResult(success=False, message="Missing API key")
+        if not self.config.api_secret:
+            return ValidationResult(success=False, message="Missing API secret")
+        try:
+            self.client.test_connection()
+            return ValidationResult(success=True, message="Validation successful.")
+        except Exception as e:
+            return ValidationResult(success=False, message=f"Validation failed: {e}")
+
+    def fetch(self):
+        self.data = IllumioData()
+        self.data.workloads = self.client.get_workloads()
+        self.data.labels = self.client.get_labels()
+        self.data.flows = self.client.get_flows()
+
+    def process(self):
+        if not self.data:
+            raise Exception("No data fetched")
+        transformed_data = self.data.transform()
+        for record in transformed_data:
+            self.send_data(record)
+
+            
+            
+if __name__ == "__main__":
+plugin = IllumioPlugin()
+options = PluginOptions()
+plugin.run(options)
+
