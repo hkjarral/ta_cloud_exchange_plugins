c6ae99662a19f17071ed2289fe0416f97fc4e515
Haider Jarral <42726887+hkjarral@users.noreply.github.com>
2023-03-17 14:58:47 -0700

    Update main.py

--- Illumio/main.py
+++ Illumio/main.py
@@ -1,48 +1,84 @@
+import json
+import logging
 import requests
-from datetime import datetime
-from typing import Dict, Any, Optional
-from ta_cloud_exchange_plugins.sdk.plugin_base import PluginBase, ValidationResult
+
+from typing import Dict, Any
+from requests.exceptions import RequestException
+from ta_cloud_exchange_plugins import Plugin, ValidationResult
 
 
-class IllumioLabelsPlugin(PluginBase):
+class IllumioLabelPlugin(Plugin):
     """
-    This plugin retrieves labels from the Illumio API.
+    This plugin retrieves labels from the Illumio API based on the provided configuration.
     """
 
-    def __init__(self, name: str, configuration: Optional[Dict[str, Any]] = None):
-        super().__init__(name, configuration)
+    def __init__(self, configuration: Dict[str, Any]):
+        super().__init__(configuration)
 
-        # API endpoint for getting labels
-    """ self.labels_api = "https://api.illumio.com/api/v2/orgs/{}/labels" """
-        self.labels_api = "{self.configuration[api_url]}/api/v2/orgs/{self.configuration[org_id]}/labels"
+        # Set up logger
+        self.logger = logging.getLogger(__name__)
+
+        # Check if required configuration parameters are present
+        if "api_url" not in self.configuration:
+            self.logger.error("API URL is missing from configuration.")
+            raise ValueError("API URL is missing from configuration.")
+        if "api_username" not in self.configuration:
+            self.logger.error("API Username is missing from configuration.")
+            raise ValueError("API Username is missing from configuration.")
+        if "api_password" not in self.configuration:
+            self.logger.error("API Password is missing from configuration.")
+            raise ValueError("API Password is missing from configuration.")
+        if "org_id" not in self.configuration:
+            self.logger.error("Organization ID is missing from configuration.")
+            raise ValueError("Organization ID is missing from configuration.")
+        if "label_id" not in self.configuration:
+            self.logger.error("Label ID is missing from configuration.")
+            raise ValueError("Label ID is missing from configuration.")
 
     def validate_configuration(self) -> ValidationResult:
         """
-        Validate the plugin configuration.
-        """
-        # Ensure the org_id is provided and is a string
-        if "org_id" not in self.configuration:
-            return ValidationResult(success=False, message="Missing org_id in configuration.")
-        elif not isinstance(self.configuration["org_id"].isdigit()):
-            return ValidationResult(success=False, message="org_id must be digits.")
-        return ValidationResult(success=True, message="Configuration validated successfully.")
+        Validates the plugin configuration.
 
-    def get_labels(self) -> Dict[str, str]:
+        Returns:
+            A ValidationResult object indicating whether the validation was successful and, if not, the reason for failure.
         """
-        Retrieve the labels from the API and return them as a dictionary.
+        if not isinstance(self.configuration["api_key"], str):
+            return ValidationResult(success=False, message="API key must be a string.")
+        if not isinstance(self.configuration["api_secret"], str):
+            return ValidationResult(success=False, message="API secret must be a string.")
+        if not isinstance(self.configuration["api_endpoint"], str):
+            return ValidationResult(success=False, message="API endpoint must be a string.")
+        if not isinstance(self.configuration["org_id"], str):
+            return ValidationResult(success=False, message="Organization ID must be a string.")
+        return ValidationResult(success=True, message="Validation successful.")
+
+    def pull_labels(self) -> Dict[str, str]:
         """
-        org_id = self.configuration["org_id"]
-        headers = {"Authorization": "Bearer " + self.get_api_key(), "Accept": "application/json"}
-        response = requests.get(self.labels_api.format(org_id), headers=headers)
-        response.raise_for_status()
+        Retrieves all labels from the Illumio API.
+
+        Returns:
+            A dictionary containing the labels and their IDs.
+        """
         labels = {}
-        for label in response.json()["results"]:
-            labels[label["id"]] = label["name"]
-        return labels
+        try:
+            response = requests.get(
+                url=f"{self.configuration['api_endpoint']}/api/v2//orgs/{self.configuration['org_id']}/labels",
+                auth=(self.configuration["api_key"], self.configuration["api_secret"]),
+                headers={"Content-Type": "application/json"},
+                timeout=30,
+            )
+            response.raise_for_status()
+            data = response.json()
+            for label in data:
+                labels[label["name"]] = label["id"]
+            return labels
+        except RequestException as e:
+            self.logger.error(f"Failed to retrieve labels from Illumio API: {e}")
+            raise ValueError(f"Failed to retrieve labels from Illumio API: {e}")
 
-    def get(self) -> Dict[str, Any]:
+    def run(self) -> None:
         """
-        Get the labels from the API and return them as a dictionary with the current time.
+        Runs the plugin.
         """
-        labels = self.get_labels()
-        return {"labels": labels, "timestamp": datetime.utcnow().isoformat()}
+        labels = self.pull_labels()
+        print(json.dumps(labels, indent=4))
