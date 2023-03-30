3e40f9bedad84e75ea76d2f5aa4bfe90d9a9e5c7
Haider Jarral <42726887+hkjarral@users.noreply.github.com>
2023-03-30 12:29:11 -0700

    Update main.py
    Test Code 2

--- Illumio/main.py
+++ Illumio/main.py
@@ -1,86 +1,169 @@
+"""Illumio Plugin providing implementation for pull and validate methods from PluginBase."""
 import json
-import logging
+import requests
+import jsonpath
+from netskope.integrations.cte.plugin_base import (
+    PluginBase,
+    ValidationResult,
+    PushResult,
+)
+from netskope.integrations.cte.models import Indicator, IndicatorType
+from netskope.integrations.cte.models.business_rule import (
+    Action,
+    ActionWithoutParams,
+)
+from netskope.common.utils import add_user_agent
+from pydantic import ValidationError
+from .utils.office365_constants import REGIONS, SERVICE_TYPES
+import uuid
 import requests
 
-from typing import Dict, Any
-from requests.exceptions import RequestException
-from netskope.integrations.cte.plugin_base import PluginBase, ValidationResult
+PLUGIN_NAME = "Illumio"
 
 
-class IllumioLabelPlugin(PluginBase):
-    """
-    This plugin retrieves labels from the Illumio API based on the provided configuration.
-    """
+class IllumioException(Exception):
+    """Office365Endpoint Exception class."""
 
-    def __init__(self, configuration: Dict[str, Any]):
-        super().__init__(configuration)
+    pass
 
-        # Set up logger
-        self.logger = logging.getLogger(__name__)
 
-        # Check if required configuration parameters are present
-        if "api_url" not in self.configuration:
-            self.logger.error("API URL is missing from configuration.")
-            raise ValueError("API URL is missing from configuration.")
-        if "api_username" not in self.configuration:
-            self.logger.error("API Username is missing from configuration.")
-            raise ValueError("API Username is missing from configuration.")
-        if "api_password" not in self.configuration:
-            self.logger.error("API Password is missing from configuration.")
-            raise ValueError("API Password is missing from configuration.")
-        if "org_id" not in self.configuration:
-            self.logger.error("Organization ID is missing from configuration.")
-            raise ValueError("Organization ID is missing from configuration.")
-        if "label_id" not in self.configuration:
-            self.logger.error("Label ID is missing from configuration.")
-            raise ValueError("Label ID is missing from configuration.")
+class IllumioPlugin(PluginBase):
+    """Illumio class template implementation."""
 
-    def validate(self, configuration) -> ValidationResult:
-        """
-        Validates the plugin configuration.
+    def handle_error(self, resp: requests.models.Response):
+        """Handle the different HTTP response code.
 
+        Args:
+            resp (requests.models.Response): Response object returned from API
+            call.
         Returns:
-            A ValidationResult object indicating whether the validation was successful and, if not, the reason for failure.
+            dict: Returns the dictionary of response JSON when the response
+            code is 200.
+        Raises:
+            HTTPError: When the response code is not 200.
         """
-        if not isinstance(self.configuration["api_password"], str):
-            return ValidationResult(success=False, message="API username must be a string.")
-        if not isinstance(self.configuration["api_username"], str):
-            return ValidationResult(success=False, message="API secret must be a string.")
-        if not isinstance(self.configuration["api_url"], str):
-            return ValidationResult(success=False, message="API url must be a string.")
-        if not isinstance(self.configuration["org_id"].isdigit()):
-            return ValidationResult(success=False, message="Organization ID must be a digits.")
-        if not isinstance(self.configuration["label_id"], str):
-            return ValidationResult(success=False, message="Label ID must be a string.")
-        return ValidationResult(success=True, message="Validation successful.")
-
-    def pull_labels(self) -> Dict[str, str]:
-        """
-        Retrieves all labels from the Illumio API.
-
-        Returns:
-            A dictionary containing the labels and their IDs.
-        """
-        labels = (self.configuration["label_id"])
-        try:
-            response = requests.get(
-                url=f"{self.configuration['api_url']}/api/v2/orgs/{self.configuration['org_id']}/labels",
-                auth=(self.configuration["api_username"], self.configuration["api_password"]),
-                headers={"Content-Type": "application/json"},
-                timeout=30,
+        err_msg = f"Response code {resp.status_code} received."
+        if resp.status_code == 200 or resp.status_code == 201:
+            try:
+                return resp.json()
+            except ValueError:
+                self.logger.error(
+                    f"{PLUGIN_NAME}: Response is not JSON format. "
+                )
+                raise IllumioException(
+                    f"{PLUGIN_NAME}: Exception occurred while parsing JSON response."
+                )
+        elif resp.status_code == 401:
+            self.logger.error(f"{PLUGIN_NAME}: {err_msg}")
+            raise IllumioException(
+                f"{PLUGIN_NAME}: Received exit code 401, Authentication Error"
             )
-            response.raise_for_status()
-            data = response.json()
-            for label in data:
-                labels[label["name"]] = label["id"]
-            return labels
-        except RequestException as e:
-            self.logger.error(f"Failed to retrieve labels from Illumio API: {e}")
-            raise ValueError(f"Failed to retrieve labels from Illumio API: {e}")
+        elif resp.status_code == 403:
+            self.logger.error(f"{PLUGIN_NAME}: {err_msg}")
+            raise IllumioException(
+                f"{PLUGIN_NAME}: Received exit code 403, Forbidden User"
+            )
+        elif resp.status_code == 404:
+            self.logger.error(f"{PLUGIN_NAME}: {err_msg}")
+            raise IllumioException(
+                f"{PLUGIN_NAME}: Received exit code 404, Not Found"
+            )
+        elif resp.status_code >= 400 and resp.status_code < 500:
+            self.logger.error(f"{PLUGIN_NAME}: {err_msg}")
+            raise IllumioException(
+                f"{PLUGIN_NAME}: Received exit code {resp.status_code}, HTTP client Error"
+            )
+        elif resp.status_code >= 500 and resp.status_code < 600:
+            self.logger.error(f"{PLUGIN_NAME}: {err_msg}")
+            raise IllumioException(
+                f"{PLUGIN_NAME}: Received exit code {resp.status_code}, HTTP server Error"
+            )
+        else:
+            self.logger.error(f"{PLUGIN_NAME}: {err_msg}")
+            raise IllumioException(
+                f"{PLUGIN_NAME}: Received exit code {resp.status_code}, HTTP Error"
+            )
 
-    def run(self) -> None:
+    def pull(self):
+        """Pull Labels from PCE"""
+
+        """Create dynamic client request > requirement for this website"""
+       # clientRequestId = str(uuid.uuid4())
+
+        """Get all content from location configured on the plugin"""
+       # region = self.configuration["region"].strip()
+       # url = f"https://endpoints.office.com/endpoints/{region}"
+       # params = {"clientRequestId": clientRequestId}
+
+        api_username = 'api_186832bf998c2ee8b'
+        api_password = 'bfe74299b44018cb2925624eb815a5007e72109e728f863771cb9b26dc0873a5'
+        api_url = 'https://illumio-pce-19.talabs.net:8443/api/v2/orgs/1'
+        org_id = '1'
+        label_id = 'quarantine'
+
+
+        api_url = (self.configuration.get(api_url) + '/api/v2/orgs' + self.configuration.get(org_id) + '/workloads')
+
+        headers = {
+            'Accept': 'application/json'
+        }
+        auth = (self.configuration.get(api_username), self.configuration.get(api_password))
+
+        response = requests.get(api_url , headers=headers, auth=auth)
+        data = self.handle_error(response)
+        indicators = []
+
+        # Parse the JSON response to extract the workloads
+        json_response = json.loads(response.text)
+
+        # Extract value of Location Label from each array of the output.
+        for i in range(0, len(json_response)):
+            labels = jsonpath.jsonpath(json_response[i], 'labels[2].value')
+
+            # Check if Location Label is set to "quarantine" if it is return public ip of workload
+            if (labels[0]) == label_id:
+                try:
+                public_ip = (jsonpath.jsonpath(json_response[i], 'public_ip'))
+                indicators.append(Indicator(value=public_ip[0],type=IndicatorType.URL,)
+                
+                except ValidationError as err:
+                        self.logger.error(
+                        message=f"{PLUGIN_NAME}: Error occurred while pulling Labels. Hence skipping {url}",
+                        details=f"Error Details: {err}",
+                        )
+        return indicators
+
+    def validate(self, configuration):
+        """Validate the Plugin configuration parameters.
+        Args:
+            data (dict): Dict object having all the Plugin configuration parameters.
+        Returns:
+            cte.plugin_base.ValidateResult: ValidateResult object with success flag and message.
         """
-        Runs the plugin.
-        """
-        labels = self.pull_labels()
-        print(json.dumps(labels, indent=4))
+        err_msg = None
+        if (
+                "api_url" not in configuration
+                or type(configuration.get(api_url)) != str
+                or not configuration["api_url"].strip()
+        ):
+            err_msg = "API URL is Required Field."
+
+        if configuration["api_url"] not in REGIONS:
+            err_msg = "Invalid URL Provided"
+
+        if (
+                "label_id" not in configuration
+                or type(configuration.get("label_id")) != str
+        ):
+            err_msg = "Label Field is required"
+
+        if not err_msg:
+            return ValidationResult(
+                success=True,
+                message=f"{PLUGIN_NAME}: Validation Successful.",
+            )
+        else:
+            self.logger.error(
+                f"{PLUGIN_NAME}: Validation error occurred, Error: {err_msg}"
+            )
+            return ValidationResult(success=False, message=err_msg)
