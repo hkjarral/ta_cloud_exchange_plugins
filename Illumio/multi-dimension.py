3b467d9e9d478634fbda8bea78bea39f70d7eb50
Haider Jarral <42726887+hkjarral@users.noreply.github.com>
2023-05-05 14:34:34 -0700

    Delete multi-dimension.py

--- Illumio/multi-dimension.py
+++
@@ -1,9 +0,0 @@
-query_string = "location:aws,app:web"
-label_dimensions = query_string.split(",")
-
-for label in label_dimensions:
-    key,value = label.split(":")
-    print(key,value)
-
-
-# params={“key”: key, “value”: value}
