0a8b11ca6cac4ab1a867d20a8c60d8a7d8831753
Haider Jarral <42726887+hkjarral@users.noreply.github.com>
2023-05-05 16:20:35 -0700

    Update __init__.py

--- Illumio/lib/illumio/__init__.py
+++ Illumio/lib/illumio/__init__.py
@@ -8,18 +8,18 @@ Copyright:
 License:
     Apache2, see LICENSE for more details.
 """
-from events import *
-from exceptions import *
-from secpolicy import *
-from util import *
-from accessmanagement import *
-from policyobjects import *
-from infrastructure import *
-from vulnerabilities import *
-from workloads import *
-from rules import *
-from explorer import *
-from pce import *
+from .events import *
+from .exceptions import *
+from .secpolicy import *
+from .util import *
+from .accessmanagement import *
+from .policyobjects import *
+from .infrastructure import *
+from .vulnerabilities import *
+from .workloads import *
+from .rules import *
+from .explorer import *
+from .pce import *
 
 from types import ModuleType
 
