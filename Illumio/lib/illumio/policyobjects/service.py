b635e2df75e93b398215c12752bc89daffac5feb
Haider Jarral <42726887+hkjarral@users.noreply.github.com>
2023-05-10 10:25:53 -0700

    Update service.py

--- Illumio/lib/illumio/policyobjects/service.py
+++ Illumio/lib/illumio/policyobjects/service.py
@@ -11,7 +11,7 @@ License:
 from dataclasses import dataclass
 from typing import List, Union
 
-from ..illumio import IllumioException
+from .lib.illumio import IllumioException
 from .lib.illumio.util import (
     JsonObject,
     MutableObject,
