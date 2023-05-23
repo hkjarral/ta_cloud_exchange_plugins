c8aa4d00287a178c7334f72f6efb1399a512f0bf
Haider Jarral <42726887+hkjarral@users.noreply.github.com>
2023-05-23 12:45:12 -0700

    Update main.py

--- Illumio/main.py
+++ Illumio/main.py
@@ -57,7 +57,7 @@ class IllumioPlugin(PluginBase):
 
         indicators = []
         indicators = self.labeltoip(pce, config["label_scope"])
-        self.logger.info(f"Illumio Plugin Successfully retrieved indicators: {indicators}")
+        self.logger.info(f"Illumio Plugin: Successfully retrieved indicators: {indicators}")
         return indicators
 
     def validate(self, data):
