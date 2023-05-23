c181c79372ee4bef8b83f02a5bb6d9580f46f445
Haider Jarral <42726887+hkjarral@users.noreply.github.com>
2023-05-23 13:09:31 -0700

    Update main.py

--- Illumio/main.py
+++ Illumio/main.py
@@ -56,8 +56,7 @@ class IllumioPlugin(PluginBase):
         pce = PolicyComputeEngine(config["api_url"], port=config["api_port"], org_id=config["org_id"])
         pce.set_credentials(config["api_username"], config["api_password"])
 
-        #indicators = []
-        indicators = self.labeltoip(pce, config["label_scope"])
+        indicators[] = self.labeltoip(pce, config["label_scope"])
         self.logger.info(f"Illumio Plugin: Successfully retrieved indicators: {indicators}")
         return indicators
 
